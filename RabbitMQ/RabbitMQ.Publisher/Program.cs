80fdce24970f7f61341275a70d7f45d4e46a5ecd
yucelaydin <yucel@4alabs.io>
2023-04-02 23:49:43 +0300

    basit rabbitmq örneği eklendi

---
+++ RabbitMQ/RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,52 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace RabbitMQ.Publisher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //RabbitMQ bağlanmak için Factory sınıfından instance alıyoruz
+            var factory = new ConnectionFactory();
+            // api.cloudamqp.com rabbitmq cloud verdiği url i factorye veriyoruz.
+            factory.Uri = new Uri("amqps://uliyuxac:GTQlxYysEK14TQz-j8F-B7NQ6_V4XHp7@rattlesnake.rmq.cloudamqp.com/uliyuxac");
+
+            // bağlantıyı açıyoruz
+            using var connection = factory.CreateConnection();
+            
+            //bağlantı üzerinden bir kanal oluşturuyoruz rabbitmq ya erişmek için.
+            var channel=connection.CreateModel();
+
+            // kuyruk oluşturuyoruz
+            // "hello-queue" kuyruk adı
+            //durable
+            ////*false olursa oluşan kuyruklar memoryde tutular rabbitmq restart olursa kuyruklar kaybolur
+            ////*true olursa fiziksel olarak kaydedilir ve kuyruklar kaybedilmez.
+            //exclusive
+            //// * false olursa farklı bir processten ulaşabilmek için.
+            //// * true olursa bu kuyhruğa sadece burdaki oluşturulan kanal üzerinden bağlanabiliriz
+            // autoDelete
+            /// *false subscriber kalmasada kuyruk kalır
+            /// *true bağlı son subscriber bağlantısıda kesilince kuyruk silinir
+            channel.QueueDeclare("hello-queue",true,false,false);
+            
+            //Kuyruğa gönderilen mesaj mesajlar byte dizisi olarak gönderilir. Bu yüzden dosya vs. de gönderilebilir.
+            string message = "Hello word";
+            // Byte olarak aldık.
+            var messageBody = Encoding.UTF8.GetBytes(message);  
+
+            // exchange kullanmıyoruz
+            channel.BasicPublish(string.Empty,"hello-queue",null,messageBody);
+
+
+            Console.WriteLine(message+ " mesajı kuyruğa gönderildi");
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
---
+++ RabbitMQ/RabbitMQ.Subscriber/Program.cs
@@ -0,0 +1,53 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace RabbitMQ.Subscriber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //RabbitMQ bağlanmak için Factory sınıfından instance alıyoruz
+            var factory = new ConnectionFactory();
+            // api.cloudamqp.com rabbitmq cloud verdiği url i factorye veriyoruz.
+            factory.Uri = new Uri("amqps://uliyuxac:GTQlxYysEK14TQz-j8F-B7NQ6_V4XHp7@rattlesnake.rmq.cloudamqp.com/uliyuxac");
+
+            // bağlantıyı açıyoruz
+            using var connection = factory.CreateConnection();
+
+            //bağlantı üzerinden bir kanal oluşturuyoruz rabbitmq ya erişmek için.
+            var channel = connection.CreateModel();
+
+            // publisher tarafında bu kuyruğu zaten oluşturduğumuz için bu satırda ilgili kuyruğa bağlanırız
+            // eğer publisherde oluşmazsa burda oluşur
+            //publisher tarafında oluştuğuna eminsek kullanmaya gerek olmaz.
+            // iki taraftada parametreler aynı olmalı
+            //            channel.QueueDeclare("hello-queue", true, false, false);
+
+
+            //subscriber(comsumer) oluşturuyoruz ve oluşturulan kanalı verdik
+            var consumer = new EventingBasicConsumer(channel);
+
+            //kanal üzerinden "hello-queue" isimli kuyruğu dinliyoruz
+            //autoAck
+            /// *true olursa kuyruktan mesaj alındıktan sonra rabbitmq ilgili mesajı siler
+            /// *false olursa  rabbitmq ya mesajı silme, işleme göre mesaj silineceğini biz bildireceğiz demiş oluruz.
+            channel.BasicConsume("hello-queue", true, consumer);
+
+            //"hello-queue" kuyruğuna bir mesaj geldiğinde bu event çalışır
+            consumer.Received += (object? sender, BasicDeliverEventArgs e) =>
+            {
+                // Publisherda byte dizisine çevirip yolladığımız mesajı burada tekrar string olarak alıyhoruz
+                var messages = Encoding.UTF8.GetString(e.Body.ToArray());
+
+                Console.WriteLine(messages);
+
+            };
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
